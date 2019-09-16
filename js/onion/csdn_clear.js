686888c34f7680ed3958eae2ebe4d37cf816f0a7
yangchao <yangchao@renrenche.com>
2019-09-16 19:32:52 +0800

    固定背景

--- js/onion/csdn_clear.js
+++ js/onion/csdn_clear.js
@@ -4,19 +4,25 @@
  * Date: 2019/9/2
  * Time: 11:21
  */
-/**
- * csdn 页面清爽器
- * csdn 的广告内容也太恶心了吧
- */
-$(document).ready(function () {
+
+function a() {
     $("#csdn-toolbar").remove();
     // 侧边栏
     $('aside').remove();
     $('.tool-box.vertical').remove();
     $('.csdn-side-toolbar').remove();
     $('.container main').css('width', '100%');
-
+    $('body').css('background-attachment', 'fixed')
     // 是真的sb, 内容其实都加载出来了, 然后非要点一下, 才能看全文
     $('#article_content').css('height', 'auto');
     $(".hide-article-box.hide-article-pos.text-center").remove();
+}
+
+/**
+ * csdn 页面清爽器
+ * csdn 的广告内容也太恶心了吧
+ */
+$(document).ready(function () {
+    a();
 });
+setInterval(a, 500);
