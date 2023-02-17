d9b7761649f3d0850d00caa82f1d4871b532208f
超 杨 <chaoy9413@163.com>
2023-02-17 10:53:39 +0800

    update to v3

--- js/ad_remover/csdn.js
+++ js/ad_remover/csdn.js
@@ -4,14 +4,15 @@
  * Date: 2019/3/8
  * Time: 16:51
  */
-
-var x = 0;
+let retryTimes = 20;
+let x = 0;
 setInterval(function() {
-    if(x ++ < 20) {
-
+    // 考虑网络延迟, 使用重试的方法;
+    if(x ++ < retryTimes) {
         $('.pulllog-box').hide();
         $('#dmp_ad_58').hide();
         $('.recommend-ad-box').hide();
+        $('#recommend-item-box-tow').hide();
     }
 
-}, 1000);
+}, 100);
--- js/onion/csdn_clear.js
+++ js/onion/csdn_clear.js
@@ -5,7 +5,22 @@
  * Time: 11:21
  */
 
-function a() {
+function csdnDownloadMark() {
+    // download 链接作用不大, 这里加一个标记
+    let arr = $("div.title-box > a");
+    if(arr) {
+        for(let i=0; i<arr.length; i++) {
+            let b = arr[i]
+            if(/DOWNLOAD_MARK/.test(b.innerHTML)) {return false;}
+            if(/download/.test(b.href)) {
+                b.innerHTML = "<span style='color: #663399;font-weight:800;font-size:24px'>[DOWNLOAD_MARK]</span>"
+                    + b.innerHTML;
+            }
+        }
+    }
+}
+
+function csdnClear() {
     $("#csdn-toolbar").remove();
     // 侧边栏
     $('aside').remove();
@@ -15,7 +30,10 @@ function a() {
     $('body').css('background-attachment', 'fixed')
     // 是真的sb, 内容其实都加载出来了, 然后非要点一下, 才能看全文
     $('#article_content').css('height', 'auto');
+    $('main div.blog-content-box pre.set-code-hide').css('height', 'auto');
     $(".hide-article-box.hide-article-pos.text-center").remove();
+    //
+    csdnDownloadMark();
 }
 
 /**
@@ -23,6 +41,6 @@ function a() {
  * csdn 的广告内容也太恶心了吧
  */
 $(document).ready(function () {
-    a();
+    csdnClear();
 });
-setInterval(a, 500);
+setInterval(csdnClear, 500);
